e596f7c83c11feb121f092da0cea3e3daea317a8
Roman Kovban <roman.kovban@gmail.com>
2024-01-13 22:19:59 +0200

    Add Google client env

--- app/components/index.ts
+++ app/components/index.ts
@@ -2,6 +2,7 @@ export { default as Link } from './link.component';
 export { default as Skeleton } from './skeleton.component';
 export { default as Spinner } from './spinner.component';
 export { default as ErrorMessage } from './error-message.component';
+export { default as Pagination } from './pagination.component';
 
 export { IssueTable, columnNames } from './issue-table.component';
 export { IssueStatusBadge } from './issue-status-badge.component';
